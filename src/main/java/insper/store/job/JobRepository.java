093f03d3b7a51e17d326a46687719e1ca12eb385
Alessi181 <tomasalessi@gmail.com>
2024-05-24 22:55:03 -0300

    Fix: Redis

--- src/main/java/insper/store/job/JobRepository.java
+++ src/main/java/insper/store/job/JobRepository.java
@@ -1,6 +1,6 @@
 package insper.store.job;
 
-import java.util.Optional;
+
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
--- src/main/java/insper/store/job/JobService.java
+++ src/main/java/insper/store/job/JobService.java
@@ -18,7 +18,7 @@ public class JobService {
     private JobRepository JobRepository;
 
 
-    @Cacheable(value = "jobs", key = "#id", unless = "#result == null")
+    @Cacheable("jobs")
     public Iterable<JobModel> readAll() {
         return JobRepository.findAll();
     }
@@ -27,7 +27,7 @@ public class JobService {
         return JobRepository.save(new JobModel(in)).to();
     }
 
-    @Cacheable(value = "job", key = "#id", unless = "#result == null")
+    @Cacheable(value = "job")
     public Job read(@NonNull String id) {
         return JobRepository.findById(id).map(JobModel::to).orElse(null);
     }
